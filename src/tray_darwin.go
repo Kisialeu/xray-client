0709ec8fc5ad6ba73f68bf3c79d2fda0c8b5a2d5
siarhei <siarhei@siarheis-mbp.speedport.ip>
2026-08-28 01:22:13 +0200

    hotfix

--- src/tray_darwin.go
+++ src/tray_darwin.go
@@ -221,8 +221,8 @@ func trayOnReady(ctx context.Context, rootCancel context.CancelFunc, logger *slo
 						}
 
 						status := "🟢  " + pName
-						bw := fmt.Sprintf(" ↓ %s/s   ↑ %s/s", humanBytes(txRate), humanBytes(rxRate))
-						tot := fmt.Sprintf(" Total ↓ %s   ↑ %s", humanBytes(float64(out)), humanBytes(float64(in)))
+						bw := fmt.Sprintf(" ↑ %s/s   ↓ %s/s", humanBytes(rxRate), humanBytes(txRate))
+						tot := fmt.Sprintf(" Total ↑ %s   ↓ %s", humanBytes(float64(in)), humanBytes(float64(out)))
 
 						if status != prevStatus {
 							mStatusLine.SetTitle(status)
